c461da74af90fb8da1dc85aa57ffadd02a2be6d1
SulthonAuliya <sulthon.mt@gmail.com>
2026-01-22 18:40:43 +0700

    feat : adjusted setup and add env.example

--- runtime/docker-entrypoint-initdb.d/002_load_data.sql
+++ runtime/docker-entrypoint-initdb.d/002_load_data.sql
@@ -1,6 +1,6 @@
 \c reporting
 
-create table public.product_events (
+create table if not exists public.product_events (
   id SERIAL PRIMARY KEY,
   evt_type character varying(255),
   user_id character varying(100),
@@ -15,7 +15,7 @@ create table public.product_events (
   last_modified timestamp default current_timestamp
 );
 
-create table public.user_events (
+create table if not exists public.user_events (
   id SERIAL PRIMARY KEY,
   evt_type character varying(255),
   user_id character varying(100),
@@ -27,62 +27,62 @@ create table public.user_events (
 );
 
 insert into public.user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('signup', '1', '2024-01-02 09:00:00', 'app', null);
+values ('signup', '1', '2025-01-02 09:00:00', 'app', null);
 
 insert into public.user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('signup', '2', '2024-01-03 11:00:00', 'web', null);
+values ('signup', '2', '2025-01-03 11:00:00', 'web', null);
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('signup', '3', '2024-01-03 08:20:00', 'web', null);
+values ('signup', '3', '2025-01-03 08:20:00', 'web', null);
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('signup', '4', '2024-01-06 12:40:00', 'web', null);
+values ('signup', '4', '2025-01-06 12:40:00', 'web', null);
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('add-payment-method', '1', '2024-01-02 09:03:00', 'app', '{"type": "card", "valid_until": "08/24"}');
+values ('add-payment-method', '1', '2025-01-02 09:03:00', 'app', '{"type": "card", "valid_until": "08/24"}');
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('add-payment-method', '2', '2024-01-03 11:13:00', 'web', '{"type": "card", "valid_until": "02/24"}');
+values ('add-payment-method', '2', '2025-01-03 11:13:00', 'web', '{"type": "card", "valid_until": "02/24"}');
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('add-payment-method', '3', '2024-01-04 08:24:00', 'web', '{"type": "card", "valid_until": "01/24"}');
+values ('add-payment-method', '3', '2025-01-04 08:24:00', 'web', '{"type": "card", "valid_until": "01/24"}');
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('add-payment-method', '3', '2024-02-14 08:24:00', 'web', '{"type": "card", "valid_until": "01/26"}');
+values ('add-payment-method', '3', '2025-02-14 08:24:00', 'web', '{"type": "card", "valid_until": "03/26"}');
 
 insert into user_events (evt_type, user_id, evt_date, platform, meta) 
-values ('add-payment-method', '4', '2024-01-06 13:01:00', 'web', '{"type": "card", "valid_until": "01/24"}');
+values ('add-payment-method', '4', '2025-01-06 13:01:00', 'web', '{"type": "card", "valid_until": "01/26"}');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '1', '1', '1', 'LOC A', '2024-01-14 08:24:00', '1', 'app', '');
+values ('borrow', '1', '1', '1', 'LOC A', '2025-07-14 08:24:00', '1', 'app', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('return', '1', '1', '2', 'LOC B', '2024-01-16 10:00:00', '1', 'app', '');
+values ('return', '1', '1', '2', 'LOC B', '2025-07-16 10:00:00', '1', 'app', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '1', '2', '2', 'LOC B', '2024-01-17 11:00:00', '2', 'web', '');
+values ('borrow', '1', '2', '2', 'LOC B', '2025-07-17 11:00:00', '2', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('return', '1', '2', '2', 'LOC B', '2024-01-17 12:00:00', '2', 'web', '');
+values ('return', '1', '2', '2', 'LOC B', '2025-07-17 12:00:00', '2', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '2', '1', '2', 'LOC B', '2024-01-20 09:00:00', '3', 'web', '');
+values ('borrow', '2', '1', '2', 'LOC B', '2025-07-20 09:00:00', '3', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('return', '2', '1', '2', 'LOC B', '2024-01-21 12:00:00', '3', 'web', '');
+values ('return', '2', '1', '2', 'LOC B', '2025-11-21 12:00:00', '3', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '2', '1', '2', 'LOC B', '2024-01-22 09:00:00', '4', 'web', '');
+values ('borrow', '2', '1', '2', 'LOC B', '2025-7-22 09:00:00', '4', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '3', '2', '1', 'LOC A', '2024-01-23 14:00:00', '5', 'app', '');
+values ('borrow', '3', '2', '1', 'LOC A', '2025-7-23 14:00:00', '5', 'app', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '4', '3', '2', 'LOC B', '2024-01-07 09:00:00', '6', 'web', '');
+values ('borrow', '4', '3', '2', 'LOC B', '2025-12-07 09:00:00', '6', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('return', '4', '3', '2', 'LOC B', '2024-01-07 12:00:00', '6', 'web', '');
+values ('return', '4', '3', '2', 'LOC B', '2025-12-07 12:00:00', '6', 'web', '');
 
 insert into product_events (evt_type, user_id, product_id, location_id, location, evt_date, transaction_id, platform, meta)
-values ('borrow', '4', '4', '2', 'LOC B', '2024-01-09 09:00:00', '7', 'web', '');
+values ('borrow', '4', '4', '2', 'LOC B', '2025-7-09 09:00:00', '7', 'web', '');
 
